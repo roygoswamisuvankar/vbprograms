487fe2b7823b1fb218ac91093efb4a4b5d0f8895
suvankar roy goswami <46588797+roygoswamisuvankar@users.noreply.github.com>
2020-10-26 10:24:34 +0530

    Create print A to Z.vb

---
+++ print A to Z.vb
@@ -0,0 +1,11 @@
+Module Module1
+
+    Sub Main()
+        Dim i As Long
+        For i = 1 To 26
+            Console.WriteLine(Chr(i + 64))
+        Next i
+        Console.ReadLine()
+    End Sub
+
+End Module
